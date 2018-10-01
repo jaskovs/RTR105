90801acc0fb280b61614a9749ae662d4848b4c7a
user <jaskovs@protonmail.ch>
2018-10-01 14:00:56 +0300

    20181001_14_00

---
+++ test_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+a = 10
+b = 1.56
+c = 'W'
+print ("a = %d"%(a))
+print ("b = %.4f"%(b))
+print ("c = %s"%(c))
+
+
+a = 20
+b = 3.49
+c = 'O'
+print ("a = %d"%(a))
+print ("b = %.4f"%(b))
+print ("c = %s"%(c))
+
